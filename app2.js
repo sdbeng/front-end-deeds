1b7c4fc3b1a06aa02a1c0cd9b76e08ebfb803f83
Sergio D. Barrera <sdbeng@gmail.com>
2020-07-21 16:54:04 -0700

    fix- createElement work fine

--- app2.js
+++ app2.js
@@ -4,10 +4,11 @@ console.log('====================================');
 //whatever user input log it as text to the p tag
 let inputBox = document.querySelector("input")
 let btn = document.querySelector("#btn")
-let log = document.getElementById("log-text")
+
 
 btn.addEventListener("click", (e) => {
     // log.textContent = e.target.value
+    let log = document.createElement("p")
     log.textContent = inputBox.value
     document.body.append(log)
     inputBox.value = ''
