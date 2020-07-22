129ffc1bdba3d0607f09de62f59bc967aeb7cf86
Sergio D. Barrera <sdbeng@gmail.com>
2020-07-22 16:06:55 -0700

    add some style

--- app2.js
+++ app2.js
@@ -4,14 +4,22 @@ console.log('====================================');
 //whatever user input log it as text to the p tag
 let inputBox = document.querySelector("input")
 let btn = document.querySelector("#btn")
-
+let count = 0
 
 btn.addEventListener("click", () => {
         
     let log = document.createElement("p")
+    console.log('inputBox.value ',inputBox.value)
     log.textContent = inputBox.value
     document.body.append(log)
     inputBox.value = ''
+
+    //hide entry
+    let miniBtn = document.createElement('button')
+    // console.log('miniBtn ',miniBtn)
+    miniBtn.style.display = "block"
+    miniBtn.innerText = 1
+    document.body.append(miniBtn)
 
 })
 
--- saved.js
+++ saved.js
@@ -1,6 +1,8 @@
 const entryForm = document.getElementById('entryForm');
 const entriesSection = document.getElementById('entries');
-const entryTextbox = document.getElementsByClassName('entry-textbox');
+// const entryTextbox = document.getElementsByClassName('entry-textbox');
+//by replacing getElementsByClassName() by querySelect() i can refactor line 21 to,
+const entryTextbox = document.querySelector('.entry-textbox');
 
 /*
   - Inside the addEntryToDom function, create a new variable named entryDiv.
@@ -16,9 +18,10 @@ function addEntryToDom(event) {
     event.preventDefault();
     const entryDiv = document.createElement('div');
     entryDiv.className = 'single-entry';
-    entryDiv.innerText = entryTextbox[0].value;
+    entryDiv.innerText = entryTextbox.value;
     entriesSection.appendChild(entryDiv);
     // console.log(entryTextbox[0].value);
+    console.log(entryTextbox.value);
 }
 
 entryForm.addEventListener('submit', addEntryToDom)
